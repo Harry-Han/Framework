eeedd6a4129fc7afe57f124f2911040d6b4a4a7b
韩春林 <chunlin.han@gaeamobile.com>
2017-04-20 15:11:07 +0800

    检查数组维度，维度不够可以增加维度

---
+++ function/arrayDepth.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * arrayDepth 函数先来检验数组的维度
+ */
+function arrayDepth(array $array){
+    $max_depth = 1;
+    foreach ($array as $value) {
+        if (is_array($value)) {
+            $depth = arrayDepth($value) + 1;
+            if ($depth > $max_depth) {
+                $max_depth = $depth;
+            }
+        }
+    }
+    return $max_depth;
+}
+
+
+$arr = array(
+    array(
+        '1' => '1111',
+        '2' => '2222',
+        '3' => '3333'
+    )
+);
+//如果维度不够的话可以进行增加维度
+if (arrayDepth($arr) < 2)
+    $dataCollector = array($dataCollector);
+
+$arrDp = arrayDepth($arr);
+var_dump($arrDp);
