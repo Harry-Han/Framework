321f13c781dec54992b05ae135c76e76610891a1
韩春林 <chunlin.han@gaeamobile.com>
2017-06-27 16:41:50 +0800

    no message

--- PHP/verCompare.php
+++ PHP/verCompare.php
@@ -20,6 +20,7 @@ function compareVersion($version1, $version2) {
     return 0;
 }
 /////////////////////////////
+////////////////////////////
 $ver1 = '14.4.1';
 $ver2 = '14.3.1';
 $result = compareVersion($ver1,$ver2);
